e5e7a1c11c804ac5d3aafc9c174bc27df45e498c
Tim.Razumov <tima.razumov@gmail.com>
2020-02-18 18:34:11 +0300

    add tests

--- calc/calc.go
+++ calc/calc.go
@@ -1,114 +1,129 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
-    "strings"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+    "errors"
 )
 
 type stack struct {
-    data []interface{}
+	data []interface{}
 }
 
-func (this *stack) top() (val interface{}) {
-    return this.data[this.size() - 1]
+func (this *stack) Top() (val interface{}) {
+	return this.data[this.Size()-1]
 }
 
-func (this *stack) pop() (val interface{}) {
-    size := this.size()
-    val = this.data[size - 1]
-    this.data = this.data[:size - 1]
-    return val
+func (this *stack) Pop() (val interface{}) {
+	size := this.Size()
+	val = this.data[size-1]
+	this.data = this.data[:size-1]
+	return val
 }
 
-func (this *stack) push(val ...interface{}) {
-    this.data = append(this.data, val...)
+func (this *stack) Push(val ...interface{}) {
+	this.data = append(this.data, val...)
 }
 
-func (this *stack) empty() bool {
-    return len(this.data) == 0
+func (this *stack) Empty() bool {
+	return len(this.data) == 0
 }
 
-func (this *stack) size() int {
-    return len(this.data)
+func (this *stack) Size() int {
+	return len(this.data)
 }
 
-func getPriority(op string) int {
-    switch op {
-        case "+", "-" :
-            return 0
-        case "*", "/" :
-            return 1
-        default:
-            return -1
-    }
+func GetPriority(op string) int {
+	switch op {
+	case "+", "-":
+		return 0
+	case "*", "/":
+		return 1
+	default:
+		return -1
+	}
 }
 
-func makeOp(l int, r int, op string) int {
-    switch op {
-        case "+" :
-            return l + r
-        case "-" :
-            return l - r
-        case "*" :
-            return l * r
-        case "/" :
-            return l / r
-        default:
-            return 0 // TODO
-    } 
+func BasicOp(l int, r int, op string) (int, error) {
+	switch op {
+	case "+":
+		return l + r, nil
+	case "-":
+		return l - r, nil
+	case "*":
+		return l * r, nil
+	case "/":
+		return l / r, nil
+	default:
+		return 0, errors.New("undef op")
+	}
 }
 
-func makeOps(prevOps *stack, nums *stack, currOp string) {
-    if !prevOps.empty() && currOp != "(" {
-        if currOp == ")" {
-            prevOp := prevOps.pop().(string);
-            for prevOp != "(" {
-                r, l := nums.pop().(int), nums.pop().(int)
-                nums.push(makeOp(l, r, prevOp))
-                prevOp = prevOps.pop().(string);
-            }
-            return
-        }
-        if prevOp := prevOps.top().(string); getPriority(prevOp) >= getPriority(currOp) {
-            prevOps.pop()
-            r, l := nums.pop().(int), nums.pop().(int)
-            nums.push(makeOp(l, r, prevOp))
-        }
-    }
-    prevOps.push(currOp)
-}
-
-func calc(expr []string) int {
-    var nums, op stack
-    for idx := 0; idx < len(expr); idx++ {
-        if num, err := strconv.Atoi(expr[idx]); err == nil {
-            nums.push(num)
-        } else {
-            makeOps(&op, &nums, expr[idx])
-            if expr[idx] == "(" {
-                if expr[idx + 1] == "+" || expr[idx + 1] == "-" {
-                    idx++
-                    if expr[idx] == "-" {
-                        expr[idx + 1] = expr[idx] + expr[idx + 1]
-                        expr[idx] = ""
-                    }
+func MakeOps(prevOps *stack, nums *stack, currOp string) error {
+	if !prevOps.Empty() && currOp != "(" {
+		if currOp == ")" {
+			prevOp := prevOps.Pop().(string)
+			for prevOp != "(" {
+				r, l := nums.Pop().(int), nums.Pop().(int)
+                if res, err := BasicOp(l, r, prevOp); err == nil {
+                    nums.Push(res)
+                } else {
+                    return errors.New("invalid expr")
                 }
+				prevOp = prevOps.Pop().(string)
+			}
+			return nil
+		}
+		if prevOp := prevOps.Top().(string); GetPriority(prevOp) >= GetPriority(currOp) {
+			prevOps.Pop()
+			r, l := nums.Pop().(int), nums.Pop().(int)
+            if res, err := BasicOp(l, r, prevOp); err == nil {
+                nums.Push(res)
+            } else {
+                return errors.New("invalid expr")
             }
-        }
-    }
-    return nums.pop().(int)
+		}
+	}
+	prevOps.Push(currOp)
+    return nil
+}
+
+func Calc(expr []string) (int, error) {
+	var nums, op stack
+	for idx := 0; idx < len(expr); idx++ {
+		if num, err := strconv.Atoi(expr[idx]); err == nil {
+			nums.Push(num)
+		} else {
+			if err := MakeOps(&op, &nums, expr[idx]); err != nil {
+                return 0, err
+            }
+			if expr[idx] == "(" {
+				if expr[idx+1] == "+" || expr[idx+1] == "-" {
+					idx++
+					if expr[idx] == "-" {
+						expr[idx+1] = expr[idx] + expr[idx+1]
+						expr[idx] = ""
+					}
+				}
+			}
+		}
+	}
+	return nums.Pop().(int), nil
 }
 
 func main() {
-    if len(os.Args) == 2 {
-        expr := "(" + os.Args[1] + ")"
-        fmt.Println(expr)
-        slice := strings.Split(expr, "") // string to slice
-        ans/*, _*/ := calc(slice)
-        fmt.Println(slice, " = ", ans)
-    } else {
-        fmt.Println("error")
-    }
+	if len(os.Args) == 2 {
+		expr := "(" + os.Args[1] + ")"
+		fmt.Println(expr)
+		slice := strings.Split(expr, "") // string to slice
+		if ans, err := Calc(slice); err == nil {
+            fmt.Println(slice, " = ", ans)
+        } else {
+            fmt.Println(err)
+        }
+	} else {
+		fmt.Println("print expr")
+	}
 }
