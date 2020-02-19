90dfffd5be1d36280d47f685d4e3fffbea8fc299
Tim.Razumov <tima.razumov@gmail.com>
2020-02-19 15:24:16 +0300

    first chan

---
+++ signer/signer.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "sync/atomic"
+	"time"
+)
+
+func StartWorker(worker job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer func() {
+		close(out)
+		wg.Done()
+	}()
+	worker(in, out)
+}
+
+func ExecutePipeline(workers ...job) {
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, MaxInputDataLen)
+	out := make(chan interface{}, MaxInputDataLen)
+	//nextBuff := make(chan interface{}, MaxInputDataLen)
+	wg.Add(1)
+	go StartWorker(workers[0], in, out, wg)
+	wg.Add(1)
+	go StartWorker(workers[1], out, in, wg)
+	/*for _, worker := range workers {
+		go worker(in, out)
+	}*/
+	wg.Wait()
+}
+
+func main() {
+	var ok = true
+	var recieved uint32
+	freeFlowJobs := []job{
+		job(func(in, out chan interface{}) {
+			out <- 1
+			time.Sleep(10 * time.Millisecond)
+			currRecieved := atomic.LoadUint32(&recieved)
+			fmt.Println("currRecieved", currRecieved)
+			if currRecieved == 0 {
+				ok = false
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for _ = range in {
+				atomic.AddUint32(&recieved, 1)
+				fmt.Println("recieved", recieved)
+			}
+		}),
+	}
+	ExecutePipeline(freeFlowJobs...)
+	if !ok || recieved == 0 {
+		fmt.Println("no value free flow - dont collect them")
+	} else {
+	    fmt.Println("ok")
+	}
+}
