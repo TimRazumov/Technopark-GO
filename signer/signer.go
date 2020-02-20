01b767d60a1b9d53cc97f18ad4633afa54a8a40a
Tim.Razumov <tima.razumov@gmail.com>
2020-02-20 13:47:02 +0300

    add hash

--- signer/signer.go
+++ signer/signer.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "sync"
-    "sync/atomic"
-	"time"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func StartWorker(worker job, in, out chan interface{}, wg *sync.WaitGroup) {
@@ -27,30 +27,70 @@ func ExecutePipeline(workers ...job) {
 	wg.Wait()
 }
 
-func main() {
-	var ok = true
-	var recieved uint32
-	freeFlowJobs := []job{
-		job(func(in, out chan interface{}) {
-			out <- 1
-			time.Sleep(10 * time.Millisecond)
-			currRecieved := atomic.LoadUint32(&recieved)
-			fmt.Println("currRecieved", currRecieved)
-			if currRecieved == 0 {
-				ok = false
-			}
-		}),
-		job(func(in, out chan interface{}) {
-			for _ = range in {
-				atomic.AddUint32(&recieved, 1)
-				fmt.Println("recieved", recieved)
-			}
-		}),
+// crc32(data)+"~"+crc32(md5(data))
+func SingleHasher(data string, out chan interface{}, mu *sync.Mutex, pWg *sync.WaitGroup) {
+	defer pWg.Done()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	var first, second string
+	go func() {
+		first = DataSignerCrc32(data)
+		wg.Done()
+	}()
+	go func() {
+		mu.Lock()
+		second = DataSignerMd5(data)
+		mu.Unlock()
+		second = DataSignerCrc32(second)
+		wg.Done()
+	}()
+	wg.Wait()
+	out <- first + "~" + second
+}
+
+func SingleHash(in, out chan interface{}) {
+	mu := &sync.Mutex{}
+	parentWg := &sync.WaitGroup{}
+	for val := range in {
+		parentWg.Add(1)
+		go SingleHasher(strconv.Itoa(val.(int)), out, mu, parentWg)
 	}
-	ExecutePipeline(freeFlowJobs...)
-	if !ok || recieved == 0 {
-		fmt.Println("no value free flow - dont collect them")
-	} else {
-	    fmt.Println("ok")
+	parentWg.Wait()
+}
+
+// string res += crc32(th+data)) (th=0..5)
+func MultiHasher(data string, out chan interface{}, pWg *sync.WaitGroup) {
+	defer pWg.Done()
+	const numTh = 6
+	hash := make([]string, numTh)
+	wg := &sync.WaitGroup{}
+	wg.Add(numTh)
+	for th := 0; th < numTh; th++ {
+		go func(idx int) {
+			hash[idx] = DataSignerCrc32(strconv.Itoa(idx) + data)
+			wg.Done()
+		}(th)
 	}
+	wg.Wait()
+	out <- strings.Join(hash, "")
+}
+
+func MultiHash(in, out chan interface{}) {
+	parentWg := &sync.WaitGroup{}
+	for val := range in {
+		parentWg.Add(1)
+		go MultiHasher(val.(string), out, parentWg)
+	}
+	parentWg.Wait()
+}
+
+func CombineResults(in, out chan interface{}) {
+	var res []string
+	for val := range in {
+		res = append(res, val.(string))
+	}
+	sort.Slice(res, func(i, j int) bool {
+		return res[i] < res[j]
+	})
+	out <- strings.Join(res, "_")
 }
